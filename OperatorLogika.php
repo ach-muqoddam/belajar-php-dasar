a34c827e2a4deaadf9db9db4be2d4609bce51882
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 17:27:46 +0700

    belajar operator logika

---
+++ OperatorLogika.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 10;
+$b = 10;
+
+// Operator AND akan TRUE jika keduanya bernilai TRUE
+var_dump($a && $b);
+
+// Operator OR akan TRUE jika salah satu atau keduanya TRUE
+var_dump($a || $b);
+
+// Operator NOT akan TRUE jika variable yang dimaksud FALSE
+var_dump(!false);
+// Operator XOR akan TRUE jika perbandingan variable salah satu TRUE atau tidak keduanya
