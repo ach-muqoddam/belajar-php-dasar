5d9ffae7150349ef2dbe62256ec4ce7ec2b1b85e
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 20:51:17 +0700

    belajar manipulasi string

--- ManipulasiString.php
+++ ManipulasiString.php
@@ -1,5 +1,34 @@
 <?php
 
+//  Manipulasi String
 $name = "Ach Muqoddam";
 
 echo "Nama Saya Adalah: " . $name . PHP_EOL;
+
+// Manipulasi String untuk konversi ke Number dan sebaliknya
+$valueString = (string)100;
+var_dump($valueString);
+
+$valueInt = (int)"100";
+var_dump($valueInt);
+
+$valueFloat = (float)1.01;
+var_dump($valueFloat);
+
+// Manipulasi String untuk Mengakses Karakter
+$name = "Adam";
+
+echo $name[0] . PHP_EOL;
+echo $name[1] . PHP_EOL;
+echo $name[2] . PHP_EOL;
+echo $name[3] . PHP_EOL;
+
+// Manipulasi String menggunakan Variable Parsing
+$namaAs = "ADAM";
+
+echo "Nama panggilan saya $namaAs, Selamat Belajar PHP" . PHP_EOL;
+
+// Manipulasi String mengggunakan Curly Brace
+$var = "var";
+
+echo "This is {$var}s " . PHP_EOL;
