b84b6b958691a04611d9bc1b07a914fefa11932c
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 17:12:48 +0700

    belajar operator perbandingan

---
+++ OperatorPerbandingan.php
@@ -0,0 +1,13 @@
+<?php
+// Opertaor perbandingan sama-dengan atau istilahnya tap jugling melakukan perubahan tipe data secara otomatis
+var_dump("10" == 10);
+
+// Operator perbandingan identik mengecek kesamaan tipe data
+var_dump("10" === 10);
+
+// Operator perbandingan kurang dari
+var_dump(10 > 9);
+var_dump(9 > 9);
+
+// Operator perbandingan kurang dari sama-dengan
+var_dump(9 >= 9);
