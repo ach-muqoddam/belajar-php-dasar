f39fbd342fd9e1b0a5ad8480cde2960a874dc621
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 16:36:15 +0700

    belajar operator aritmatika

---
+++ OperatorAritmatika.php
@@ -0,0 +1,18 @@
+<?php
+
+// Operator penjumlahan, dll..
+$a = 10;
+$b = 10;
+
+$result = $a + $b;
+var_dump($result);
+
+// Mengkonversi menjadi bilangin negative
+$OpertatorNegative = -$result;
+var_dump($OpertatorNegative);
+$OperatorPositive = +$result;
+var_dump($OperatorPositive);
+
+// Operator Modulo
+$resultModulo = 100 % 3;
+var_dump($resultModulo);
