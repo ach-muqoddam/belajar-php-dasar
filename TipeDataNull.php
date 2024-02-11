96ff01eeba0f49cfec9f00c186d8fa92f15ab5af
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 10:19:00 +0700

    belajar is_null, isset, unset

--- TipeDataNull.php
+++ TipeDataNull.php
@@ -12,6 +12,29 @@ echo $age;
 echo "\n";
 
 // mengecek variabel NULL
+// cara pertama
 echo "Is variable NULL? ";
+echo "\n";
+echo "variabel name?";
 var_dump(is_null($name));
+echo "\n";
+echo "variabel age?";
 var_dump(is_null($age));
+echo "\n";
+
+// cara kedua
+// lebih direkomendasikan
+echo "saya ingin mengecek variabel yang kosong: ";
+echo "\n";
+echo "variabel name?";
+var_dump(isset($name));
+echo "\n";
+echo "variabel age?";
+var_dump(isset($age));
+
+// menghapus varibel
+// apabila variabel dihapus maka variabel tersebut sudah tidak ada dan tidak bisa di gunakan kembali
+$contoh = "ini contoh variabel yang mau dihapus";
+unset($contoh);
+
+echo $contoh;
