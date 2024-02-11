db25888bbccbb721cb6f2f5f8ec54f9f61468cf5
Ach Muqoddam <ach.adam04@gmail.com>
2024-02-11 19:35:41 +0700

    belajar Operator Increment dan Decrement

---
+++ OperatorIncrementDecrement.php
@@ -0,0 +1,11 @@
+<?php
+
+// Operator Post Increment ($a++) ialah kembalikan $a lalu naikkan 1 angka
+// Operator Pre Increment (++$a) ialah  naikkan 1 angka, lalu kembalikan $a
+// Operator Post Decrement ($a--) ialah kembalikan $a lalu turunkan 1 angka
+// Operator Pre Decrement (--$a) ialah  turunkan 1 angka, lalu kembalikan $a
+$a = 10;
+// $a = $a + 1;
+// $b = $a;
+$b = ++$a; // sama halnya dengan seperti contoh penulisan diatas
+var_dump($a);
